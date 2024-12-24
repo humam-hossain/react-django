4aefc75d5b253f9e0e7557ff09157b210b27e2f7
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2024-12-24 09:37:17 +0600

    [INIT] project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Book Website</h1>
+      <div>
+        <input type="text" placeholder="Boook Title" />
+        <input type="number" placeholder="Release Date" />
+        <button>Add Book</button>
+      </div>
+    </>
+  )
+}
+
+export default App
