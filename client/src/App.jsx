3087734360d5905219f4f417e351395fde674fb8
Muhammed Humam Hossain <humam.hossain.e@gmail.com>
2024-12-25 23:09:08 +0600

    [DONE] CRUD

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,57 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [books, setBooks] = useState([])
+  const [title, setTitle] = useState("")
+  const [releaseYear, setReleaseYear] = useState("")
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
+  const fetchBooks = async () => {
+    const response = await fetch("http://localhost:8000/api/books/")
+    const data = await response.json()
+    // console.log(data)
+    setBooks(data)
+  }
+
+  const addBook = async () => {
+    const bookData = {
+      title: title,
+      release_year: releaseYear
+    }
+
+    try{
+      const response = await fetch("http://localhost:8000/api/books/create/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(bookData)
+      })
+
+      const data = await response.json()
+      setBooks((prev) => [...prev, data])
+    }catch(error){
+      console.log(error)
+    }
+  }
 
   return (
     <>
       <h1>Book Website</h1>
       <div>
-        <input type="text" placeholder="Boook Title" />
-        <input type="number" placeholder="Release Date" />
-        <button>Add Book</button>
+        <input type="text" placeholder="Boook Title" onChange={(e) => setTitle(e.target.value)} />
+        <input type="number" placeholder="Release Year" onChange={(e) => setReleaseYear(e.target.value)} />
+        <button onClick={addBook}>Add Book</button>
       </div>
+      {books.map((book) => (
+        <div key={book.id}>
+          <p>Title: {book.title} Release Year: {book.release_year}</p>
+        </div>
+      ))}
     </>
   )
 }
--- server/server/settings.py
+++ server/server/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
-    'rest_framework',
+    "rest_framework",
+    "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
